ac5647e8f8614ab14b15987fb038d6e7d65d05ac
mazwimyeza <mazwimyeza1@gmail.com>
2019-09-20 22:11:33 +0200

    created services classes for profile and campaigns

--- FrontEnd/Butterfly/src/app/services/campaign.service.ts
+++ FrontEnd/Butterfly/src/app/services/campaign.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Campaign } from '../models/campaign/campaign.module';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CampaignService {
 
-  constructor() { }
+  campaignsUrl: string = 'http://localhost/campaigns';
+  campaignLimit: string = '?_limit=10'
+
+  constructor(private http: HttpClient) { }
+
+  getCampaigns(): Observable<Campaign[]>{
+    return this.http.get<Campaign[]>(`${this.campaignsUrl}${this.campaignLimit}`);
+  }
+
+  getCampaign(campaign: Campaign): Observable<Campaign>{
+    return this.http.get<Campaign>(`${this.campaignsUrl}/${campaign.tagline}`);
+  }
 }
--- FrontEnd/Butterfly/src/app/services/profile.service.ts
+++ FrontEnd/Butterfly/src/app/services/profile.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Profile } from '../models/profile/profile.module';
 import { Observable } from 'rxjs';
+import { Campaign } from '../models/campaign/campaign.module';
 
 @Injectable({
   providedIn: 'root'
@@ -18,7 +19,14 @@ export class ProfileService {
   }
 
   getProfile(profile: Profile):Observable<Profile>{
-    return this.http.get<Profile>(`${this.profilesUrl}/${profile.id}`)
+    return this.http.get<Profile>(`${this.profilesUrl}/${profile.id}`);
   }
 
+  getProfileByName(name: string):Observable<Profile>{
+    return this.http.get<Profile>(`${this.profilesUrl}/${name}`);
+  }
+
+  getProfileCampaigns(profile: Profile):Observable<Campaign[]>{
+    return this.http.get<Campaign[]>(`${this.profilesUrl}/${profile.id}/campaigns`);
+  }
 }
