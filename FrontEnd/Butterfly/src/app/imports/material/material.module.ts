a385451a434e58e8ccb93dfbbbf3a94d8fd94932
mazwimyeza <mazwimyeza1@gmail.com>
2019-09-23 01:53:33 +0200

    campaign enhancement

--- FrontEnd/Butterfly/src/app/imports/material/material.module.ts
+++ FrontEnd/Butterfly/src/app/imports/material/material.module.ts
@@ -5,6 +5,7 @@ import { MatButtonModule } from '@angular/material/button'
 import { MatToolbarModule } from '@angular/material/toolbar'
 import { MatExpansionModule } from '@angular/material/expansion'
 import { MatTooltipModule } from '@angular/material/tooltip'
+import { MatProgressBarModule } from '@angular/material/progress-bar'
 
 const MaterialComponents = [
   MatCardModule,
@@ -12,7 +13,8 @@ const MaterialComponents = [
   MatButtonModule, 
   MatToolbarModule,
   MatExpansionModule,
-  MatTooltipModule
+  MatTooltipModule,
+  MatProgressBarModule
 ];
 
 @NgModule({
