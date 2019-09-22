7e6d0e2113f0aa0fe15a6f038b0b910e3f9bdd48
mazwimyeza <mazwimyeza1@gmail.com>
2019-09-22 13:46:47 +0200

    hover color on options, landing page.

--- FrontEnd/Butterfly/src/app/app-routing.module.ts
+++ FrontEnd/Butterfly/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { ProfileComponent } from './components/profile/profile.component';
 import { HomeComponent } from './components/home/home.component';
 import { OpeningComponent } from './components/opening/opening.component';
+import { CampaignComponent } from './components/campaign/campaign.component';
 
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'open', component: OpeningComponent},
-  {path: 'profiles', component: ProfileComponent}
+  {path: 'profiles', component: ProfileComponent},
+  {path: 'campaigns', component: CampaignComponent}
 ];
 
 @NgModule({
